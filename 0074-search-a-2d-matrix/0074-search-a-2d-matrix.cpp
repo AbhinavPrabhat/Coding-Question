ff69720e482d5915bd38f75459a7d8d52b8a858a
Abhinav Prabhat <81673273+AbhinavPrabhat@users.noreply.github.com>
2023-09-29 21:41:24 +0530

    Time: 3 ms (70.9%), Space: 9.8 MB (9.84%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,27 +1,56 @@
 class Solution {
 public:
-    int binarySearch(vector<int> arr, int n, int k){
-        int left =0; 
-        int right = n-1;
-        while(left<=right){
-            int mid = left +(right-left)/2;
+/*better appraoch applying binary search on each row */
+    // bool binarySearch(vector<int> arr,int m, int k){
+    //     int left =0; 
+    //     int right = m-1;
+    //     while(left<=right){
+    //         int mid = left +(right-left)/2;
 
-            if(arr[mid]==k) return mid; 
-            else if(arr[mid]>k) right = mid-1;
-            else left = mid+1;
-        }
-        return -1;
-    }
+    //         if(arr[mid]==k) return true; 
+    //         else if(arr[mid]>k) right = mid-1;
+    //         else left = mid+1;
+    //     }
+    //     return false;
+    // }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size(); 
-        int n = matrix[0].size();
+
+        int n = matrix.size(); 
+        int m = matrix[0].size();
+
+        /*better approach 
         for(int i =0; i<m;i++){
-            if(binarySearch(matrix[i],n,target)!= -1){
-            return true; 
-        }
+           // convert the bianry search function to integer type 
+            // if(binarySearch(matrix[i],m,target)!=-1){
+            //     return true; 
+            // }
+            if(matrix[i][0]<= target && target <= matrix[i][m-1]){
+                return binarySearch(matrix[i],m,target);
+            }
+            
 
         }
         
         return false; 
+
+        */
+
+        /*Optimal approach -- flattent the 2d matrix  by not actually falattenig it */
+
+        // to conevert 1d index to 2d index form
+        // row = /m; 
+        // column = %m
+
+        int left =0;
+        int right = n*m-1; 
+        while(left<=right){
+            int mid = left +(right-left)/2;
+            int row = mid/m; 
+            int col = mid%m; 
+            if(matrix[row][col]==target) return true; 
+            else if (matrix[row][col]<target)  left = mid+1;
+            else right = mid-1; 
+        }
+    return false; 
     }
 };
